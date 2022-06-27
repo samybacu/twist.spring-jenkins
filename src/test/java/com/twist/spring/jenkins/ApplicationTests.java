7096d0a5d94181c99f2859b840fc5c0955e72c2b
bacu <samy@localhost.localdomain>
2022-06-27 11:55:58 +0200

    first commit to integration prj

---
+++ src/test/java/com/twist/spring/jenkins/ApplicationTests.java
@@ -0,0 +1,24 @@
+package com.twist.spring.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		
+		logger.info("test executed");
+		
+		assertEquals(true, true);
+		
+	}
+
+}
